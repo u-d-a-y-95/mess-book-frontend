08fd31a604354577eb3756e0fe2b4c1723e0965f
Saiful Islam Uday <uday@UDay.local>
2022-06-06 22:29:58 +0600

    active route css added

--- src/components/navMenu.js
+++ src/components/navMenu.js
@@ -1,16 +1,24 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
-const NavMenu = ({ path, Icon,label }) => {
+const NavMenu = ({ path, Icon, label }) => {
   return (
-    <Link to={path}>
-      <div className="border-b-2 border-gray-200">
-        <span className="w-full text-gray-600 hover:bg-sky-400 py-4 px-4 hover:text-white flex justify-start items-center">
-          <Icon className="h-5" />
-          <span className="ml-2">{label}</span>
-        </span>
-      </div>
-    </Link>
+    <NavLink to={path}>
+      {({ isActive }) => (
+        <div className="border-b-2 border-gray-200">
+          <span
+            className={`w-full text-gray-600 py-4 px-4 flex justify-start items-center ${
+              isActive
+                ? "bg-sky-400 text-white font-bold"
+                : "hover:bg-sky-200 hover:text-sky-600 hover:font-bold "
+            }`}
+          >
+            <Icon className="h-5" />
+            <span className="ml-2">{label}</span>
+          </span>
+        </div>
+      )}
+    </NavLink>
   );
 };
 
-export default NavMenu
+export default NavMenu;
--- src/layouts/leftbar.js
+++ src/layouts/leftbar.js
@@ -1,17 +1,15 @@
 import { CalendarIcon } from "@heroicons/react/solid";
 import { UserGroupIcon } from "@heroicons/react/solid";
-import { ClipBoardIcon } from "@heroicons/react/solid";
-import { Link } from "react-router-dom";
 import NavMenu from "../components/navMenu";
 
 const LeftBar = () => {
   return (
     <div className="min-w-[250px] h-full shadow-lg bg-white">
       <div className="">
-            <NavMenu label="Meals" path="/meals" Icon={CalendarIcon}/>
-            <NavMenu label="Users" path="/users" Icon={UserGroupIcon}/>
-            <NavMenu label="Notes" path="/notes" Icon={CalendarIcon}/>
-            <NavMenu label="Profile" path="/profile" Icon={CalendarIcon}/>
+        <NavMenu label="Meals" path="/meals" Icon={CalendarIcon} />
+        <NavMenu label="Users" path="/users" Icon={UserGroupIcon} />
+        <NavMenu label="Notes" path="/notes" Icon={CalendarIcon} />
+        <NavMenu label="Profile" path="/profile" Icon={CalendarIcon} />
       </div>
     </div>
   );
